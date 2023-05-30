7816560e36deb51c901633be6622d31dd5004c70
FurkanOzek <fur_ozek@hotmail.com>
2023-05-30 22:04:25 +0300

    Siren sesi

--- app/src/main/java/com/furkanozek/imhereproject/MainActivity.java
+++ app/src/main/java/com/furkanozek/imhereproject/MainActivity.java
@@ -8,22 +8,68 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+import androidx.appcompat.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
     SharedPreferences sharedPreferences;
-    static Boolean isNoticed = false ;
+    static Boolean isNoticed = false;
+    private MediaPlayer mediaPlayer;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        sharedPreferences = getSharedPreferences(EntranceScreen.MyPREFERENCES, Context.MODE_PRIVATE);
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sharedPreferences = getSharedPreferences(EntranceScreen.MyPREFERENCES, Context.MODE_PRIVATE);
 
+        mediaPlayer = MediaPlayer.create(this, R.raw.sirensesi);
+
+        imageView = findViewById(R.id.imageView);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playSiren();
+            }
+        });
 
     }
+
+
+    private void playSiren() {
+        if (!mediaPlayer.isPlaying()) {
+            mediaPlayer.start();
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    if (mediaPlayer.isPlaying()) {
+                        mediaPlayer.pause();
+                        mediaPlayer.seekTo(0);
+                    }
+                }
+            }, 28 * 1000);  // 30 seconds
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null) {
+            if (mediaPlayer.isPlaying()) {
+                mediaPlayer.stop();
+            }
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+
 
     public void helpMe(View view){
 
